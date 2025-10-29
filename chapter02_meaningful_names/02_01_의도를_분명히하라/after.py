45bc6f9572e772668d288c1ac2b2a5c8093c005a
mjkang307 <minjeon.kang@gmail.com>
2025-10-29 13:47:24 +0900

    Delete chapter02_meaningful_names/02_01_의도를_분명히하라 directory

--- chapter02_meaningful_names/02_01_의도를_분명히하라/after.py
+++
@@ -1,6 +0,0 @@
-def getFlaggedCells():
-    flagged_cells = []
-    for cell in game_board:
-        if cell.status == FLAGGED:
-            flagged_cells.append(cell)
-    return flagged_cells
--- chapter02_meaningful_names/02_01_의도를_분명히하라/before.py
+++
@@ -1,6 +0,0 @@
-def getThem():
-    result = []
-    for x in theList:
-        if x[0] == 4:
-            result.append(x)
-    return result
